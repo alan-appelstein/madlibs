e519e020f6773c8d780ce60a77f7b842f28c0067
ada <ada@localhost.localdomain>
2013-06-18 12:15:59 -0400

    first cut of madlibs

---
+++ madlibs1.rb
@@ -0,0 +1,19 @@
+puts "enter a noun"
+noun1 = gets.chomp
+
+puts 'enter a noun'
+noun2 = gets.chomp
+
+puts 'enter verb'
+verb1 = gets.chomp
+
+puts 'enter adverb'
+adverb1 = gets.chomp
+
+puts 'enter adjective'
+adj1 = gets.chomp
+
+puts 'enter pronoun'
+pronoun1 = gets.chomp
+
+puts "once upon a time there was a #{noun1} and a #{noun2} trying to #{verb1} while #{adverb1} the #{adj1} but #{pronoun1} work"
